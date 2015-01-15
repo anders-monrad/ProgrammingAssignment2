9b3a18dab935293e087f7a1c335648af4ca756e5
Anders Monrad <anders.monrad@gmail.com>
2015-01-15 22:54:07 +0100

    add working implementation and tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R implements a CacheMatrix object which "decorates" a matrix with the ability to cache the result of matrix operations.
+## One such operation is the inverse matrix calculation implemented in the cacheSolve function
 
-## Write a short comment describing this function
-
+## "Factory" function to create CacheMatrix decorator object
 makeCacheMatrix <- function(x = matrix()) {
-
+    solvedMatrix <- NULL
+    set <- function( y ) {
+        x <<- y
+        solvedMatrix <<- NULL ## new matrix - reset cached.
+    } 
+    get <- function() {
+        x
+    }
+    setSolvedMatrix <- function(smatrix) {
+        solvedMatrix <<- smatrix
+    }
+    getSolvedMatrix <- function() {
+        solvedMatrix
+    }
+    ## return list representing the CacheMatrix object
+    list(set=set, get=get, setSolvedMatrix=setSolvedMatrix,getSolvedMatrix=getSolvedMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## calculate the inverse of the CacheMatrix object. 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  solvedMatrix <- x$getSolvedMatrix()
+  if(!is.null(solvedMatrix)) {
+    message("getting cached solved matrix")
+    return(solvedMatrix)
+  }
+  data <- x$get()
+  solvedMatrix <- solve(data, ...)
+  x$setSolvedMatrix(solvedMatrix)
+  solvedMatrix
 }
+
+?solve()
---
+++ test_cachematrix.R
@@ -0,0 +1,21 @@
+## test script from Bryan
+## https://class.coursera.org/rprog-010/forum/thread?thread_id=288
+
+## first script: test cacheSolve repeatedly
+x <- matrix(rnorm(160000),400,400)
+x_ <- makeCacheMatrix(x)
+for (i in 1:1000) {
+  x__ <- cacheSolve(x_)
+}
+
+## second script: test solve repeatedly
+x <- matrix(rnorm(160000),400,400)
+for (i in 1:1000) {
+  x_ <- solve(x)
+}
+
+## third script: verify equality
+x <- matrix(rnorm(160000),400,400)
+x_ <- makeCacheMatrix(x)
+cacheSolve(x_)
+print(identical(x_$getInv(),solve(x)))
